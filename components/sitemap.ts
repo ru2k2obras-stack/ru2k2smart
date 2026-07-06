6571abe9dc6002abc80038652a48c3c2a5dcadb0
Horacio <ru2k2obras@gmail.com>
2026-07-06 16:23:57 -0300

    Agregar sitemap y robots

--- components/sitemap.ts
+++ components/sitemap.ts
@@ -1,16 +1,12 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-
   return [
-
     {
       url: "https://www.ru2k2smart.com.ar",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
-
   ];
-
 }
