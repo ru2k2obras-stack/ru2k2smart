8fd0799fda3c36b8a2aa9ca21a53358f4bd780ca
Horacio <ru2k2obras@gmail.com>
2026-07-06 16:44:42 -0300

    Agregar sitemap y robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,10 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
+    rules: [{
       userAgent: "*",
       allow: "/",
-    },
-
+    }],
     sitemap: "https://www.ru2k2smart.com.ar/sitemap.xml",
   };
 }
